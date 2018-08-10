6878173b77c94c5dbf67a23978822bfe5acdbfa5
Foster Stule <camodude5@yahoo.com>
2018-08-10 11:57:12 -0400

    added a chat component and class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ForumsComponent } from './forums/forums.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { UsersComponent } from './users/users.component';
+import { ChatComponent } from './chat/chat.component';
+import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ForumsComponent,
-    ScheduleComponent
+    ScheduleComponent,
+    UsersComponent,
+    ChatComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
