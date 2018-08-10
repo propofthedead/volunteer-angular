16853b0fee869516ea58ee1a0a584016aeebae24
Foster Stule <camodude5@yahoo.com>
2018-08-10 11:24:15 -0400

    added a json class to read in data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ForumsComponent } from './forums/forums.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ForumsComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
