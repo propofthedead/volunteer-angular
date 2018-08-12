9d32937a2f1970b77f2df05c8c79680b135575cc
Foster Stule <camodude5@yahoo.com>
2018-08-12 13:36:16 -0400

    implemented json class

--- src/app/json.ts
+++ src/app/json.ts
@@ -1,2 +1,11 @@
 export class Json {
+    Result: string;
+    Message: string;
+    Data: any;
+    Error: any;
+
+    constructor(){
+        this.Result="ok";
+        this.Message="Success";
+    }
 }
