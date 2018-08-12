3592397f83751e6c8e98fecdd54c432ae84294bb
Foster Stule <camodude5@yahoo.com>
2018-08-12 13:52:52 -0400

    added a few fuctions to retrieve and fill a loggin

--- src/app/logincheck.service.ts
+++ src/app/logincheck.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { User } from './users/user';
+import { Router } from '../../node_modules/@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogincheckService {
+LoggedUser: User;
 
-  constructor() { }
+logInUser(user:User):void{
+  this.LoggedUser=user;
+  console.log(user.Username + " is logged in");
+}
+logOutUser():void{
+  this.LoggedUser=null;
+  console.log("the user is logged out");
+}
+
+reroute():void{
+  this.router.navigateByUrl
+}
+  constructor(private router: Router) { }
 }
