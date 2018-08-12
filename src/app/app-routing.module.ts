0b06dc3f07b33b6ad5ee3b18b9e1cbca2a2914ba
Foster Stule <camodude5@yahoo.com>
2018-08-12 13:53:02 -0400

    added some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ForumsComponent } from './forums/forums.component';
+import { LoginComponent } from './users/login/login.component';
+import { CreateComponent } from './users/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path: 'home', component: ForumsComponent},
+  {path: 'user/login', component: LoginComponent},
+  {path: 'user/create', component: CreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
