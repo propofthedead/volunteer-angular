e8fd5e6c0590b0bf82dbe3b601ff9542c8646b84
Foster Stule <camodude5@yahoo.com>
2018-08-12 13:36:46 -0400

    implement comment and forum classes

--- src/app/forums/comments.ts
+++ src/app/forums/comments.ts
@@ -1,2 +1,11 @@
+import { User } from "../users/user";
+import { Forum } from "./forum";
+
 export class Comments {
+    Id:number;
+    UserId: number;
+    User: User;
+    Content: string;
+    Forum: Forum;
+    ForumId: number;
 }
--- src/app/forums/forum.ts
+++ src/app/forums/forum.ts
@@ -1,2 +1,11 @@
+import { User } from "../users/user";
+import { Comments } from "./comments";
+
 export class Forum {
+    Id: number;
+    UserId: number;
+    User: User;
+    Comments: Comments[];
+    Title: string;
+    Content: string;
 }
