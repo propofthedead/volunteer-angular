e13c1aa9c9ec8f6094b1362e14f0c1cf35981219
Foster Stule <camodude5@yahoo.com>
2018-08-12 13:37:00 -0400

    implement even and user class

--- src/app/schedule/event.ts
+++ src/app/schedule/event.ts
@@ -1,2 +1,12 @@
+import { User } from "../users/user";
+
 export class Event {
+    Id: number;
+    UserId: number;
+    User: User;
+    Volunteers: User[];
+    Description: string;
+    WantedAmountVolunteers: number;
+    AmtVolunteers: number;
+    Attendance: User[];
 }
--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    Id: number;
+    Username: string;
+    Password: string;
+    Firstname: string;
+    Lastname: string;
+    Email: string;
+    Phone: string;
+    IsVolunteer: boolean;
+    IsAdmin: boolean;
+    Hours: number;
 }
