84d75fb428c784a5c5a11063afa80b0d8e5c7ff2
Foster Stule <camodude5@yahoo.com>
2018-08-12 13:36:29 -0400

    implemented chat class

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,2 +1,11 @@
+import { User } from "../users/user";
+import { Message } from "./message";
+
 export class Chat {
+    Id: Number;
+    UserIds: Number[];
+    Users: User[];
+    MessageIds: number[];
+    Messages: Message[];
+
 }
