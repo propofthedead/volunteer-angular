2b73c5548dcdaf6d2d704afc8c9b5f0499765deb
Foster Stule <camodude5@yahoo.com>
2018-08-13 12:30:44 -0400

    added some functionality to the login component

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { LogincheckService } from '../../logincheck.service';
+import { Router } from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User
+
+  login():void{
+
+  }
+  register(): void{
+    this.router.navigateByUrl('/user/create')
+  }
+
+  constructor(private Logsvc: LogincheckService, private router:Router) { }
 
   ngOnInit() {
   }
