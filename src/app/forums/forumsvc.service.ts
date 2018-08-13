e3ff0351c0a372c9f74832a581cb43cd7a0360f0
Foster Stule <camodude5@yahoo.com>
2018-08-13 11:51:30 -0400

    added functionality

--- src/app/forums/forumsvc.service.ts
+++ src/app/forums/forumsvc.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from '../../../node_modules/rxjs';
+import { Json } from '../json';
+import { Forum } from './forum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForumsvcService {
 
-  constructor() { }
+  url: "http://localhost:58042/Forums/"
+
+  get(id: number) : Observable<Json>{
+    return this.http.get(this.url+"get/"+id) as Observable<Json>;
+  }
+  list(): Observable<Json>{
+    return this.http.get(this.url+"list") as Observable<Json>;
+  }
+
+  create(forum: Forum) : Observable<Json>{
+    return this.http.post(this.url+"create", forum) as Observable<Json>;
+  }
+
+  edit( forum: Forum) : Observable<Json>{
+    return this.http.post(this.url+"change", forum) as Observable<Json>;
+  }
+
+  remove(forum: Forum) : Observable<Json>{
+    return this.http.post(this.url+"delete", forum) as Observable<Json>;
+  }
+  constructor(private http: HttpClient) { }
 }
