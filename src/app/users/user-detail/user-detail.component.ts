f9af62a1362017bbbd9c578bd584e19d024dc910
Foster Stule <camodude5@yahoo.com>
2018-08-23 19:57:44 -0400

    added functionality

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersvcService } from '../usersvc.service';
+import { User } from '../user';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+
+  constructor(private Usesvc: UsersvcService, private Router: Router, private Routed: ActivatedRoute) { }
 
   ngOnInit() {
+    let id= this.Routed.snapshot.params.id;
+    this.Usesvc.get(id)
+    .subscribe(resp=>{
+      this.user=resp.Data;
+    })
   }
 
 }
