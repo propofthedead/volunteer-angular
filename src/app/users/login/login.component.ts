d4857a369dd9cba35768066f94652d8e51d35be5
Foster Stule <camodude5@yahoo.com>
2018-08-23 19:34:14 -0400

    added a error catch to login

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { LogincheckService } from '../../logincheck.service';
 import { Router } from '../../../../node_modules/@angular/router';
+import { UsersvcService } from '../usersvc.service';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +12,27 @@ import { Router } from '../../../../node_modules/@angular/router';
 export class LoginComponent implements OnInit {
 
   user: User
+  message:string;
+
 
   login():void{
-
+    this.Usesvc.login(this.user.Username,this.user.Password)
+    .subscribe(resp=>{
+      let us= resp.Data;
+      if(us==null){
+        this.message="User does not exist";
+      }
+      else{
+        this.Logsvc.logInUser(us);
+        this.router.navigateByUrl('/home');
+      }
+    })
   }
   register(): void{
     this.router.navigateByUrl('/user/create')
   }
 
-  constructor(private Logsvc: LogincheckService, private router:Router) { }
+  constructor(private Logsvc: LogincheckService, private router:Router, private Usesvc:UsersvcService) { }
 
   ngOnInit() {
   }
