d0d6ae234e819bae2e41a6ac876e6343220bd515
Foster Stule <camodude5@yahoo.com>
2018-08-23 20:09:59 -0400

    added a login check

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersvcService } from '../usersvc.service';
 import { User } from '../user';
 import { Router, ActivatedRoute } from '@angular/router';
+import { LogincheckService } from '../../logincheck.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -20,9 +21,10 @@ export class UserDetailComponent implements OnInit {
     let id= this.user.Id
     this.Router.navigateByUrl('/user/delete/:'+id);
   }
-  constructor(private Usesvc: UsersvcService, private Router: Router, private Routed: ActivatedRoute) { }
+  constructor(private Usesvc: UsersvcService, private Router: Router, private Routed: ActivatedRoute, private LoginSvc:LogincheckService) { }
 
   ngOnInit() {
+    this.LoginSvc.checkUser();
     let id= this.Routed.snapshot.params.id;
     this.Usesvc.get(id)
     .subscribe(resp=>{
