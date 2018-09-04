18a6ea62f54592a755f6968022a64f1b6f0e9c00
Foster Stule <camodude5@yahoo.com>
2018-09-04 19:21:33 -0400

    added more routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ import { ScheduleComponent } from './schedule/schedule.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { UserRemoveComponent } from './users/user-remove/user-remove.component';
 import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import {EventDetailComponent} from './schedule/event-detail/event-detail.component';
+import {EventCreateComponent} from './schedule/event-create/event-create.component';
+import {EventDeleteComponent} from './schedule/event-delete/event-delete.component';
+import{EventEditComponent} from './schedule/event-edit/event-edit.component';
 
 const routes: Routes = [  
   {path:'',redirectTo:'home',pathMatch:'full'},
@@ -19,7 +23,13 @@ const routes: Routes = [
   {path: 'events', component:ScheduleComponent},
   {path: 'user/edit/:id', component: UserEditComponent},
   {path: 'user/remove/:id', component:UserRemoveComponent},
-  {path: 'user/detail/:id', component: UserDetailComponent}
+  {path: 'user/detail/:id', component: UserDetailComponent},
+
+  {path: 'events/detail/:id', component: EventDetailComponent},
+  {path: 'events/create', component: EventCreateComponent},
+  {path: 'events/remove/:id', component: EventDeleteComponent},
+  {path: 'events/edit/:id',component: EventEditComponent}
+
 ];
 
 @NgModule({
