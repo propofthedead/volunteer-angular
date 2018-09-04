c966e54d131a4d29c26c03bcc3002dfe313cae67
Foster Stule <camodude5@yahoo.com>
2018-09-04 19:22:14 -0400

    added basic functionality

--- src/app/schedule/event-edit/event-edit.component.ts
+++ src/app/schedule/event-edit/event-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsvcService } from '../eventsvc.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Event } from '../event';
 
 @Component({
   selector: 'app-event-edit',
@@ -6,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-edit.component.css']
 })
 export class EventEditComponent implements OnInit {
+  
+  event:Event
 
-  constructor() { }
+  change():void{
+    this.eventsvc.edit(this.event);
+    this.router.navigateByUrl('/event/list');
+  }
+  constructor(private eventsvc: EventsvcService, private routed: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
   }
