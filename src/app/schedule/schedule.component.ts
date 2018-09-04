d33a1a548155f0f5f15c42deb4ac2a1c4aa83299
Foster Stule <camodude5@yahoo.com>
2018-09-04 19:21:48 -0400

    added some functionality

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsvcService } from './eventsvc.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-schedule',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  events: Event[];
+
+  constructor(private Eventsvc:EventsvcService, private router:Router) { }
 
   ngOnInit() {
+    this.Eventsvc.list()
+    .subscribe(resp=>{
+      this.events=resp.Data;
+      console.log(resp);
+    })
   }
 
 }
